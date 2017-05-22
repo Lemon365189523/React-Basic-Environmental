53df058582960593f01a4e13ec133e15a857b0a5
KADFWJ <930135305@qq.com>
2017-05-22 09:16:22 +0800

    配置项目，下载后要重新配置.babelrc文件

---
+++ app/component.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import "./component.css";
+import styles from "./index.less";
+
+const Cpt = () => (
+    <div>
+        <h1 className="test">我是盖世英雄！sssss</h1>
+        <h1 className={styles.test2}>sdfadfadsf</h1>
+    </div>
+);
+export default Cpt;
